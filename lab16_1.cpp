1118d1f824cb07c3653df9c75023c10bee0d9af4
Krittaphat <krittaphat_su@cmu.ac.th>
2025-01-31 16:36:36 +0700

    lab16_1.cpp completed

--- lab16_1.cpp
+++ lab16_1.cpp
@@ -32,3 +32,47 @@ int main(){
 	findColSum(dPtr,sum2,N,M); 
 	showData(sum2,1,M);
 }
+
+void showData(double *dPtr , int N , int M){
+    cout << fixed << setprecision(2);
+    for(int i = 0; i < N*M; i++){
+        cout << dPtr[i];
+        if((i+1) %M == 0)cout << endl;
+        else cout << " ";
+    }
+}
+
+void randData(double *dPtr , int N , int M){
+    for(int i = 0; i < N*M; i++){
+        dPtr[i] = (rand() %101) * 0.01;
+    }
+}
+
+void findRowSum(const double *dPtr , double *sum1 , int N , int M){
+    int j = 0 ;
+    for (int k = 0 ; k < N ; k++){
+        sum1[k] = 0 ;
+    }
+    
+    for (int i = 0 ; i < N*M ; i++){
+        sum1[j] += dPtr[i] ;
+        if ((i+1) %M == 0){
+            j = j+1 ;
+        }
+    }
+}
+
+void findColSum(const double *dPtr , double *sum1 , int N , int M){
+    int j = 0 ;
+    for (int k = 0 ; k < M ; k++){
+        sum1[k] = 0 ;
+    }
+    
+    for (int i = 0 ; i < N*M ; i++){
+        sum1[j] += dPtr[i] ;
+        j = j + 1 ;
+        if ((i+1) %M == 0){
+            j = 0 ;
+        }
+    }
+}
